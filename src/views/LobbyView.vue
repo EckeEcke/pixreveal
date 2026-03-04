0e7563809bd8632f034ed899e7bd063bd9bdae54
Christian Eckardt <eckardt.christian@web.de>
2026-03-05 00:58:18 +0100

    add loading animation

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -2,13 +2,13 @@
   <div>
     <h1>Lobby</h1>
     {{ onlineStore.currentRoomId }}
-    {{  onlineStore.client }}
     <PlayerDisplay
       v-for="player in players"
       :key="player.playerId"
       :name="player.username"
       :avatar-index="player.avatarIndex"
     />
+
     <button
       v-if="players && players.length > 1 && onlineStore.isHost"
       class="btn-outline"
@@ -16,6 +16,9 @@
     >
       START GAME
     </button>
+    <LoadingAnimation
+      :text="onlineStore.isHost ? 'WAITING FOR PLAYERS' : 'WAITING FOR HOST'"
+    />
   </div>
 </template>
 
@@ -25,14 +28,17 @@ import PlayerDisplay from "@/components/PlayerDisplay.vue";
 import { useOnlineStore } from "@/stores/online";
 import { useRouter } from "vue-router";
 import { useGameStore } from "@/stores/game";
+import LoadingAnimation from "@/components/LoadingAnimation.vue";
 
-const { rounds } = useGameStore()
+const { rounds } = useGameStore();
 
 const onlineStore = useOnlineStore();
-const router = useRouter()
+const router = useRouter();
 
 const players = computed(() => onlineStore.playersOnline);
 onMounted(() => {
-    if (players.value.length <= 0) router.push('/')
-})
+  if (players.value.length <= 0) router.push("/");
+});
 </script>
+
+<style scoped></style>
