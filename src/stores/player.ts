7a8cf477b70dee403bccd7e8d78f680aeaf9e4c4
Christian Eckardt <eckardt.christian@web.de>
2026-03-06 17:43:44 +0100

    reset online store after game, cleanup, more drawings

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -9,7 +9,6 @@ export const usePlayerStore = defineStore("player", () => {
   const playerName: Ref<string | undefined> = ref(undefined);
   const avatarIndex: Ref<number | undefined> = ref(undefined);
   const points: Ref<number> = ref(0);
-  const playersOnline: Ref<Player[]> = ref([]);
   const correctAnswers = ref(0)
 
   const setUser = (user: { username: string; avatar: number }) => {
@@ -18,12 +17,6 @@ export const usePlayerStore = defineStore("player", () => {
     points.value = 0;
     correctAnswers.value = 0;
   };
-
-  const removePlayer = (memberId: string) => {
-    playersOnline.value.filter((p) => p.playerId !== memberId);
-  };
-
-  const resetPlayersOnline = () => playersOnline.value = []
 
   const addPoints = (earnedPoints: number) => {
     points.value += earnedPoints;
@@ -34,11 +27,8 @@ export const usePlayerStore = defineStore("player", () => {
     playerName,
     avatarIndex,
     points,
-    playersOnline,
     correctAnswers,
     setUser,
     addPoints,
-    removePlayer,
-    resetPlayersOnline
   };
 });
