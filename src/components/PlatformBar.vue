e6084565a8b28aa4ed0d5407c42964ad98a29484
Christian Eckardt <eckardt.christian@web.de>
2026-03-16 00:10:02 +0100

    add platform links

---
+++ src/components/PlatformBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="social-bar">
+    <a
+      href="https://www.facebook.com/profile.php?id=61580781216710"
+      target="_blank"
+      class="btn-outline"
+      title="Follow on Facebook"
+    >
+      <Icon icon="streamline-pixel:logo-social-media-facebook-circle" />
+    </a>
+
+    <a
+      href="https://www.youtube.com/@EckeEcke/shorts"
+      target="_blank"
+      class="btn-outline"
+      title="Subscribe on YouTube"
+    >
+      <Icon icon="pixel:youtube" />
+    </a>
+
+    <a
+      href="https://www.tiktok.com/@pixreveal.com"
+      target="_blank"
+      class="btn-outline"
+      title="Follow on TikTok"
+    >
+      <Icon icon="pixel:tiktok" />
+    </a>
+
+    <a
+      href="https://github.com/EckeEcke"
+      target="_blank"
+      class="btn-outline"
+      title="View on GitHub"
+    >
+      <Icon icon="pixel:github" />
+    </a>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+
+<style scoped>
+.social-bar {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 16px;
+  margin-top: 16px;
+}
+
+.btn-outline {
+  padding: 8px;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  border: none;
+  color: var(--white);
+  filter: drop-shadow(0 0 8px var(--neon-social));
+  opacity: 0.8;
+  text-decoration: none;
+  transition: all 0.2s ease;
+}
+
+.social-bar .btn-outline:hover {
+  background: var(--neon-social);
+  box-shadow: 0 0 20px var(--white);
+  opacity: 1;
+  transform: translateY(-2px);
+}
+</style>
