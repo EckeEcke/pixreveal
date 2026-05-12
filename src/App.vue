5c6082831b4b0692fd7feec14661b9053ba7e6f2
Christian Eckardt <eckardt.christian@web.de>
2026-05-12 15:21:37 +0200

    remove crt filter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Analytics />
-  <div class="crt">
+  <div>
     <div class="pixelCon">
       <div
         v-for="n in 80"
@@ -183,32 +183,5 @@ onBeforeUnmount(() => {
   100% {
     opacity: 0;
   }
-}
-
-.crt {
-  position: relative;
-}
-
-.crt::before {
-  content: " ";
-  display: block;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background:
-    linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%),
-    linear-gradient(
-      90deg,
-      rgba(255, 0, 0, 0.06),
-      rgba(0, 200, 100, 0.02),
-      rgba(0, 0, 255, 0.06)
-    );
-  z-index: 2;
-  background-size:
-    100% 2px,
-    3px 100%;
-  pointer-events: none;
 }
 </style>
