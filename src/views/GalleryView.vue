3a74f82899ae080bc4f8295687baec9473b99d7e
Christian Eckardt <eckardt.christian@web.de>
2026-05-16 22:02:43 +0200

    reverse user drawings to show newest first

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -39,7 +39,7 @@ import InfoBox from "@/components/game-ui/InfoBox.vue";
 
 const configStore = useConfigStore();
 
-const userDrawings = computed(() => configStore.ugcDrawings);
+const userDrawings = computed(() => configStore.ugcDrawings.reverse());
 
 const selectedDrawingIndex = ref(0);
 
