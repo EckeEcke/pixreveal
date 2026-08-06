9fedc0def9865edb5087c0149db1946386c87d6b
Christian Eckardt <eckardt.christian@web.de>
2026-08-06 09:39:23 +0200

    small fix for daily challenge

--- src/views/DailyView.vue
+++ src/views/DailyView.vue
@@ -45,7 +45,7 @@
       <AnswerButtons
         :hasAnswered="hasAnswered && !playerStore.isCreatorMode"
         :answers="rounds[currentRoundIndex].options"
-        :inputDisabled="showTransition || !isRevealing"
+        :inputDisabled="showTransition"
         @answered="handleAnswer"
       />
     </section>
