7e7732901ee3ad69e465f42780144ff6f957c217
Christian Eckardt <eckardt.christian@web.de>
2026-08-09 20:41:34 +0200

    fix partner link

--- src/views/PartnersView.vue
+++ src/views/PartnersView.vue
@@ -30,9 +30,9 @@
                 <strong>{{ highlight.title }}:</strong> {{ highlight.text }}
               </li>
             </ul>
-            <ButtonLinkPrimary :link="partner.url" target="_blank">
+            <a :href="partner.url" target="_blank" class="link-btn-primary">
               VISIT {{ partner.name.toUpperCase() }} &rarr;
-            </ButtonLinkPrimary>
+            </a>
           </div>
         </div>
       </section>
@@ -199,5 +199,33 @@ h2 {
   text-align: center;
   margin-top: 32px;
   color: var(--color-secondary);
+}
+
+.link-btn-primary {
+  display: flex;
+  place-items: center;
+  justify-content: center;
+  gap: 4px;
+  background: var(--primary);
+  padding: 12px 16px;
+  border: none;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: black;
+  font-family: inherit;
+  font-weight: 900;
+  font-size: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  box-shadow:
+    rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px,
+    rgba(0, 0, 0, 0.3) 0 -3px 0 inset;
+}
+
+.link-btn-primary:hover {
+  filter: brightness(1.1);
+  transform: translateY(-4px) scale(1.05);
 }
 </style>
