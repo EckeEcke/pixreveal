31b15b720bf960672b38f08d6bccec8ad895eaad
Christian Eckardt <eckardt.christian@web.de>
2026-08-13 00:51:36 +0200

    test legacy joke url

--- vite.config.ts
+++ vite.config.ts
@@ -39,13 +39,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-    server: {
-      proxy: {
-        '/api-jokes': {
-          target: 'https://jokeapi.dev',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api-jokes/, '')
-        }
-      }
-    },
 });
